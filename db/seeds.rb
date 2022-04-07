abcc045333836748e40b29b1db7c9b0dca2ac55b
Victor Maklakov <maklakowiktor@gmail.com>
2022-04-07 10:19:27 +0300

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,9 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# seed_file = Rails.root.join('db', 'seeds.yml')
-# config = YAML::load_file(seed_file)
-# Project.create!(config)
+seed_file = Rails.root.join('db', 'seeds', 'projects.yml')
+config = YAML::load_file(seed_file)
+Project.create!(config)
 
 seed_file = Rails.root.join('db', 'seeds', 'todos.yml')
 config = YAML::load_file(seed_file)
